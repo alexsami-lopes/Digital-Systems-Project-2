b3558017ba9dd5b3312aa2248d666c9ac9e2018d
Alexsami Lopes <103523809+alexsami-lopes@users.noreply.github.com>
2023-04-12 12:57:42 -0300

    Cleaning code in initialize_LCD

--- initialize_LCD.c
+++ initialize_LCD.c
@@ -12,62 +12,15 @@
 #define LCD_D7  27               //Data pin 7
 
 
-void clearDisplay(){
-	digitalWrite(LCD_E, HIGH);
-
-	digitalWrite(LCD_RS, LOW);
-	digitalWrite(LCD_D4, LOW);
-	digitalWrite(LCD_D5, LOW);
-	digitalWrite(LCD_D6, LOW);
-	digitalWrite(LCD_D7, LOW);
-
-	digitalWrite(LCD_E, LOW);
-	delay(1);
-	digitalWrite(LCD_E, HIGH);
-	delay(1);
-	digitalWrite(LCD_E, LOW);
-	
-
-	digitalWrite(LCD_D4, HIGH);
-	digitalWrite(LCD_E, LOW);
-	delay(1);
-	digitalWrite(LCD_E, HIGH);
-
-	delay(1);
-    digitalWrite(LCD_E, LOW);
-}
 
 
-int lcd = 0;
+int display = 0;
 
 int main (void)
 {
-	wiringPiSetup();
-	pinMode(NUM, OUTPUT);
-	pinMode(LCD_RS, OUTPUT);
-	pinMode(LCD_E, OUTPUT);
-	pinMode(LCD_D4, OUTPUT);
-	pinMode(LCD_D5, OUTPUT);
-	pinMode(LCD_D6, OUTPUT);
-	pinMode(LCD_D7, OUTPUT);	
-    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+   wiringPiSetup();
+   display = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+   lcdPuts(display, "Wiring OP - LCD");
+   return 0;
 
-//	clearDisplay();
-
-	for(;;){
-
-		digitalWrite(NUM, HIGH);
-		delay(1000);
-		digitalWrite(NUM, LOW);
-		delay(1000);	
-        lcdPosition(lcd, 0, 0);
-        lcdPuts(lcd, "Funciona!!!!");
-		delay(1000);
-       
-	}
-	
-	//lcdPosition(lcd, 0, 0);
-    //lcdPuts(lcd, "Funciona!!!!");
-	//delay(1000);
-	return 0;
 }
